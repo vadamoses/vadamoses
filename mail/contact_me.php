e5af86851014a4d161ae4c47aff919477a500f43
vadamoses <vadagbasa@gmail.com>
2020-02-15 18:24:58 +0100

    next commit

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'info@flanagancleaning.nl';
+$to = 'vadagbasa@gmail.com';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@flanagancleaning.nl\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
